235e15a62a7bbcbea2118743e1301730f75748f0
Gabriella <gabriella_pimentel1@my.vcccd.edu>
2024-07-03 11:23:31 -0400

    lab

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
 def splitlist(numbers):
+    if not numbers:
+        return None, []
+    min_index = numbers.index(min(numbers))
+    numbers[0], numbers[min_index] = numbers[min_index], numbers[0]
+    return numbers [0], numbers[1:]
+
     """
     ########################################
     Code Your Program here
